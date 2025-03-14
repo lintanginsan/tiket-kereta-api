6e4b07717dc63c211ccc657dcad25220a966d02f
lintanginsan <141606706+lintanginsan@users.noreply.github.com>
2025-03-14 22:20:37 +0700

    update.env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com','www.refugee-action.org.uk'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  experimental: {
+    missingSuspenseWithCSRError: false,
+  },
+  staticPageGenerationTimeout: 120,
 };
 
 export default nextConfig;
